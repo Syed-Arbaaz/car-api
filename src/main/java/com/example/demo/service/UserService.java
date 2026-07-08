7c1067a193638b3358806bd1dec8acb788db9f75
Arbaaz <syedarbaazmohiuddin123@gmail.com>
2026-07-07 23:57:33 -0500

    Edit email verify link

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +33,9 @@ public class UserService {
 
     @Autowired
     private EmailService emailService;
+
+    @Value("${app.base-url}")
+    private String baseUrl;
 
    public User register(RegisterRequest request){
 
@@ -69,7 +73,7 @@ public class UserService {
     verificationTokenRepository.save(verificationToken);
     
     String verificationLink =
-        "http://localhost:8080/auth/verify?token="
+        baseUrl + "/auth/verify?token="
         + token;
 
         emailService.sendVerificationEmail(savedUser.getEmail(), verificationLink);
